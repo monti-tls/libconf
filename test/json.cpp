e85869f2f081f5fcbcafdd59f1b2357d2f5201c1
Alex <monti@etud.insa-toulouse.fr>
2015-08-31 19:56:36 +0200

    Added license

--- test/json.cpp
+++ test/json.cpp
@@ -1,36 +1,65 @@
+/* This file is part of libconf.
+ *
+ * libconf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libconf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libconf.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "json.h"
 #include "json_template.h"
 
 #include <sstream>
 
+//! You can add custom loading rules as showed below.
+//! First, create a class inheriting the json::UserElement class (it
+//!   has to reside in the json namespace also).
+//! This class must define two functions :
+//!   - void extract(Node*), which must process the data in the given node
+//!     (you can of course use another template here).
+//!   - Node* synthetize(), whih must create a JSON tree representing the owned data.
+//! An instance of this class will be created for each variable that must be loaded,
+//!    and must provide a constructor taking a reference to the handled type.
+//! Secondly, you must specialize the Terminal<> class to use the newly integrated one
+//!   (this is really straigthforward as you can see below).
 namespace json
 {
     template <typename U, typename V>
-    class PairElement : public Element
+    class PairElement : public UserElement
     {
     public:
         PairElement(std::pair<U, V>& ref) :
             m_ref(ref)
         {}
         
-        Element::Type type() const
-        { return Element::User; }
-        
         void extract(Node* node) const
         {
+            // Type-check the array, throwing a json::Exception instance in case
+            //   of a mismatch.
             if (node->type() != Node::Array)
                 throw Exception(node, "json::PairElement: type mismatch");
             
+            // Downcast safely the node to check its size.
             ArrayNode* arr = node->downcast<ArrayNode>();
             if (arr->size() != 2)
                 throw Exception(node, "json::PairElement: size mismatch, expecting 2 for a pair");
             
-            Terminal<U> first(m_ref.first);
-            first.extract(arr->at(0));
-            Terminal<V> second(m_ref.second);
-            second.extract(arr->at(1));
+            // Use another template to read in the array.
+            Template tpl;
+            tpl.bind_array(m_ref.first);
+            tpl.bind_array(m_ref.second);
+            tpl.extract(node);
         }
         
+        //! Here we don't care about synthetization.
         Node* synthetize() const
         { return 0; }
         
@@ -38,6 +67,8 @@ namespace json
         std::pair<U, V>& m_ref;
     };
     
+    //! Specialize the Terminal<> class to expose the new type to the json::Template
+    //!   system.
     template<typename U, typename V>
     class Terminal<std::pair<U, V> > : public PairElement<U, V>
     {
@@ -54,7 +85,7 @@ int main()
     std::string val =
     "{\n"
     "   \"a\" : 123,\n"
-    "   \"p\" : [ 1, \"yolo\"]\n"
+    "   \"p\" : [1, \"yolo\"]\n"
     "}"
     ;
     

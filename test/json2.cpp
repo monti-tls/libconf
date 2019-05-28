8f313fe0135dc4e5f48a774698d3d4f55b36625d
Alexandre Monti <alex.oct.monti@gmail.com>
2019-05-28 13:42:08 +0200

    Updated dates in license headers

--- test/json2.cpp
+++ test/json2.cpp
@@ -1,6 +1,6 @@
 /* This file is part of libconf.
  *
- * Copyright (c) 2015, Alexandre Monti
+ * Copyright (c) 2015 - 2019, Alexandre Monti
  *
  * libconf is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
